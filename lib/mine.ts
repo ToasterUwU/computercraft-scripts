bf4510c5824e66a06180b337cee03374ca8ba4a4
ToasterUwU <Aki@ToasterUwU.com>
2024-07-09 15:30:08 +0200

    very basic vein mining function, can definetly be optimized

---
+++ lib/mine.ts
@@ -0,0 +1,44 @@
+import { getBlockNameDown, getBlockNameFront, getBlockNameUp } from "./inspect";
+
+const veinBlacklist = ["minecraft:air", "minecraft:stone", "minecraft:deepslate", "minecraft:dirt"]
+
+function mineVeinPart(blockType: string) {
+    print("Above: ", getBlockNameUp())
+    if (getBlockNameUp() == blockType) {
+        turtle.digUp()
+        turtle.up()
+        mineVeinPart(blockType)
+        turtle.down()
+    }
+
+    print("Down: ", getBlockNameDown())
+    if (getBlockNameDown() == blockType) {
+        turtle.digDown()
+        turtle.down()
+        mineVeinPart(blockType)
+        turtle.up()
+    }
+
+    for (let i = 0; i < 4; i++) {
+        print("In Front: ", getBlockNameFront())
+        if (getBlockNameFront() == blockType) {
+            turtle.dig()
+            turtle.forward()
+            mineVeinPart(blockType)
+            turtle.back()
+        }
+        turtle.turnLeft()
+    }
+}
+
+// Will mine vein of whatever is in front of it and return to the starting position, dont use this when facing something that isnt a vein
+export function mineVein() {
+    let veinType = getBlockNameFront()
+    if (veinType == "" || veinType in veinBlacklist) {
+        return false
+    }
+
+    print("Mining Vein of type: ", veinType)
+
+    mineVeinPart(veinType)
+}
